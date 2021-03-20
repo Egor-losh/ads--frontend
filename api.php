ce83254f786480419e581b6119332092f7560aeb
Общий профиль ptpit <ptpit@pl1.edu>
2021-03-20 09:51:09 +0500

    jiodwpqdj

--- api.php
+++ api.php
@@ -1,14 +1,29 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;dbname=ads_db','root','root');
-$query = $pdo -> prepare('INSERT INTO test (id) value (:id)');
-$query -> bindValue(':id',1);
-$query -> execute();
-if(isset($_GET['ads'])){
-    echo 'все объявления';
+
+$pdo = new PDO(
+    'mysql:host=localhost;dbname=ads_db',
+    'root','root');
+
+if (isset($_GET['add'])) {
+
+    $text = $_POST['text'];
+    $name = $_POST['name'];
+    $phone = $_POST['phone'];
+    
+    $query = $pdo -> prepare('INSERT INTO ads 
+    (text, name, phone) 
+    value (:text, :name, :phone)');
+    $query -> bindValue(':text', $text);
+    $query -> bindValue(':name', $name);
+    $query -> bindValue(':phone', $phone);
+    $query -> execute();
 }
-else if(isset($_GET['add'])){
-    echo 'добавить объявление';
-}
-else if(isset($_GET['id'])){
-    echo 'показать объявление';
+
+else if (isset($_GET['all'])) {
+
+    $query = $pdo -> query("SELECT * FROM ads");
+    $query = $query -> fetchAll(PDO::FETCH_ASSOC);
+    header("Content-type: application/json; charset=utf-8");
+
+    echo json_encode($query);
 }
---
+++ api_add.php
@@ -0,0 +1,9 @@
+<?php
+$text = $_POST['text'];
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$query = $pdo -> prepare('INSERT INTO ads (text, name, phone) value (:text, :name, :phone)');
+$query -> bindValue(':text', $text);
+$query -> bindValue(':name', $name);
+$query -> bindValue(':phone', $phone);
+$query -> execute();
